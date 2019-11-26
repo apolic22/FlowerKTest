f6c3f463947c894b5055f21a7882d4275141ede8
apolic <antepolic22@gmail.com>
2019-11-26 16:06:28 +0100

    SecondCommit

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AComponent } from './pages/a/a.component';
+import { BComponent } from './pages/b/b.component';
+import { CComponent } from './pages/c/c.component';
+
+
+// tslint:disable-next-line:max-line-length
+import { MatTabsModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule, MatMenuModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponent,
+    BComponent,
+    CComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RoutingModule,
+    MatMenuModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    CommonModule,
+    MatTabsModule,
+    MatSidenavModule,
+    // NgbModule
+  ],
+  exports: [
+    MatMenuModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ WebClient/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent implements OnInit {
+  @Output() sidenavClose = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onSidenavClose = () => {
+    this.sidenavClose.emit();
+  }
+
+  public onSidenavCloseLogin = () => {
+    this.sidenavClose.emit();
+  }
+
+}
