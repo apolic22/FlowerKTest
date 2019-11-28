324818ef91340c71a6ff89985ffe09c57a8a3ef3
apolic <antepolic22@gmail.com>
2019-11-28 16:46:24 +0100

    refactor gallery

--- WebClient/src/app/pages/b/b.component.ts
+++ WebClient/src/app/pages/b/b.component.ts
@@ -12,4 +12,64 @@ export class BComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  // tslint:disable-next-line:member-ordering
+  images = [
+    {
+      src: 'assets/img/image1.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image2.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image3.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image4.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image5.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image6.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image7.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image8.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      src: 'assets/img/image9.jpg',
+      title: 'Lorem Ipsum',
+      // tslint:disable-next-line:max-line-length
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+
+  ];
+
 }
