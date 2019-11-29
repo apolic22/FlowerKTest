d7c7695db111528474228a78d162f8619ff2e9c8
apolic <antepolic22@gmail.com>
2019-11-29 16:27:26 +0100

    Something new

--- WebClient/src/app/pages/home/home.component.ts
+++ WebClient/src/app/pages/home/home.component.ts
@@ -4,21 +4,83 @@ import { first } from 'rxjs/operators';
 import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 
-@Component({templateUrl: 'home.component.html'})
+@Component({
+  templateUrl: 'home.component.html',
+  styleUrls: ['./home.component.css']
+})
 export class HomeComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
+    mobile = false;
 
     constructor(private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
+
+    images = [
+      {
+        src: 'assets/img/image1.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image2.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image3.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image4.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image5.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image6.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image7.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image8.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'assets/img/image9.jpg',
+        title: 'Lorem Ipsum',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+
+    ];
 
     event_list = [
         {
           event: ' Event 1',
           eventLocation: 'Bangalore',
           eventDescription: 'In bangalore, first event is going to happen. Please be careful about it',
-          img: 'https://picsum.photos/900/500?random&t=1',
+          img: 'assets/img/image2.jpg',
           eventStartDate: new Date('2019/05/20'),
           eventEndingDate: new Date('2019/05/24')
         },
@@ -26,7 +88,7 @@ export class HomeComponent implements OnInit {
           event: ' Event 2',
           eventLocation: 'Dubai',
           eventDescription: 'Dubai is another place to host so,e, first event is going to happen. Please be careful about it',
-          img: 'https://picsum.photos/900/500?random&t=3',
+          img: 'assets/img/image2.jpg',
           eventStartDate: new Date('2019/07/28'),
           eventEndingDate: new Date('2019/07/30')
         },
@@ -34,7 +96,7 @@ export class HomeComponent implements OnInit {
           event: ' Event 3',
           eventLocation: 'New York',
           eventDescription: 'NewYork sits on top of event hosting',
-          img: 'https://picsum.photos/900/500?random&t=4',
+          img: 'assets/img/image2.jpg',
           eventStartDate: new Date('2020/05/20'),
           eventEndingDate: new Date('2020/05/24')
         },
@@ -42,7 +104,7 @@ export class HomeComponent implements OnInit {
           event: ' Event 4',
           eventLocation: 'Singapore',
           eventDescription: 'Singapore is another great hosting city',
-          img: 'https://picsum.photos/900/500?random&t=6',
+          img: 'assets/img/image2.jpg',
           eventStartDate: new Date('2018/05/20'),
           eventEndingDate: new Date('2018/05/24')
         },
@@ -50,7 +112,7 @@ export class HomeComponent implements OnInit {
           event: ' Event 5',
           eventLocation: 'Berlin',
           eventDescription: 'Berlin is best place to hang on',
-          img: 'https://picsum.photos/900/500?random&t=7',
+          img: 'assets/img/image4.jpg',
           eventStartDate: new Date('2017/07/10'),
           eventEndingDate: new Date('2017/08/14')
         },
@@ -58,7 +120,7 @@ export class HomeComponent implements OnInit {
           event: ' Event 6',
           eventLocation: 'Mumbai',
           eventDescription: 'Mumbai is hub of startups',
-          img: 'https://picsum.photos/900/500?random&t=8',
+          img: 'assets/img/image5.jpg',
           eventStartDate: new Date(),
           eventEndingDate: new Date()
         },
@@ -66,19 +128,23 @@ export class HomeComponent implements OnInit {
           event: ' Event 7',
           eventLocation: 'Barcelona',
           eventDescription: 'Barcelona is another good city',
-          img: 'https://picsum.photos/900/500?random&t=6',
+          img: 'assets/img/image2.jpg',
           eventStartDate: new Date(),
           eventEndingDate: new Date()
         },
       ];
 
-      upcoming_events =  this.event_list.filter( event => event.eventStartDate > new Date());
-      past_events = this.event_list.filter(event => event.eventEndingDate < new Date());
-      current_events =  this.event_list.filter( event => (event.eventStartDate >= new Date() && (event.eventEndingDate <= new Date())));
+      // upcoming_events =  this.event_list.filter( event => event.eventStartDate > new Date());
+      // past_events = this.event_list.filter(event => event.eventEndingDate < new Date());
+      // current_events =  this.event_list.filter( event => (event.eventStartDate >= new Date() && (event.eventEndingDate <= new Date())));
 
 
     ngOnInit() {
         this.loadAllUsers();
+
+        if (window.screen.width < 420) { // 768px portrait
+          this.mobile = true;
+        }
     }
 
     deleteUser(id: number) {
