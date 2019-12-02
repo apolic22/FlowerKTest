bd6df64ec5e24b52a311cd5adc7f19992c9fe011
apolic <antepolic22@gmail.com>
2019-12-02 15:41:58 +0100

    test2_commit

--- WebClient/src/app/pages/home/home.component.ts
+++ WebClient/src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ImageServiceService } from './../../dummyServices/image-service.service';
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
@@ -13,134 +14,43 @@ export class HomeComponent implements OnInit {
     users: User[] = [];
     mobile = false;
 
-    constructor(private userService: UserService) {
+    images: { src: string; title: string; description: string; }[];
+
+    constructor(private userService: UserService, private imagesService: ImageServiceService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
-
-    images = [
-      {
-        src: 'assets/img/image1.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image2.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image3.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image4.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image5.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image6.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image7.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image8.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-      {
-        src: 'assets/img/image9.jpg',
-        title: 'Lorem Ipsum',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      },
-
-    ];
 
     event_list = [
         {
           event: ' Event 1',
           eventLocation: 'Bangalore',
           eventDescription: 'In bangalore, first event is going to happen. Please be careful about it',
-          img: 'assets/img/image2.jpg',
+          img: 'assets/images/rose.png',
           eventStartDate: new Date('2019/05/20'),
           eventEndingDate: new Date('2019/05/24')
         },
-         {
-          event: ' Event 2',
-          eventLocation: 'Dubai',
-          eventDescription: 'Dubai is another place to host so,e, first event is going to happen. Please be careful about it',
-          img: 'assets/img/image2.jpg',
-          eventStartDate: new Date('2019/07/28'),
-          eventEndingDate: new Date('2019/07/30')
-        },
-         {
-          event: ' Event 3',
-          eventLocation: 'New York',
-          eventDescription: 'NewYork sits on top of event hosting',
-          img: 'assets/img/image2.jpg',
-          eventStartDate: new Date('2020/05/20'),
-          eventEndingDate: new Date('2020/05/24')
-        },
-         {
-          event: ' Event 4',
-          eventLocation: 'Singapore',
-          eventDescription: 'Singapore is another great hosting city',
-          img: 'assets/img/image2.jpg',
-          eventStartDate: new Date('2018/05/20'),
-          eventEndingDate: new Date('2018/05/24')
-        },
-        {
-          event: ' Event 5',
-          eventLocation: 'Berlin',
-          eventDescription: 'Berlin is best place to hang on',
-          img: 'assets/img/image4.jpg',
-          eventStartDate: new Date('2017/07/10'),
-          eventEndingDate: new Date('2017/08/14')
-        },
-        {
-          event: ' Event 6',
-          eventLocation: 'Mumbai',
-          eventDescription: 'Mumbai is hub of startups',
-          img: 'assets/img/image5.jpg',
-          eventStartDate: new Date(),
-          eventEndingDate: new Date()
-        },
-        {
-          event: ' Event 7',
-          eventLocation: 'Barcelona',
-          eventDescription: 'Barcelona is another good city',
-          img: 'assets/img/image2.jpg',
-          eventStartDate: new Date(),
-          eventEndingDate: new Date()
-        },
+        //  {
+        //   event: ' Event 2',
+        //   eventLocation: 'Dubai',
+        //   eventDescription: 'Dubai is another place to host so,e, first event is going to happen. Please be careful about it',
+        //   img: 'assets/images/rose2.jpg',
+        //   eventStartDate: new Date('2019/07/28'),
+        //   eventEndingDate: new Date('2019/07/30')
+        // },
+        //  {
+        //   event: ' Event 3',
+        //   eventLocation: 'New York',
+        //   eventDescription: 'NewYork sits on top of event hosting',
+        //   img: 'assets/images/rose3.jpg',
+        //   eventStartDate: new Date('2020/05/20'),
+        //   eventEndingDate: new Date('2020/05/24')
+        // }
       ];
 
-      // upcoming_events =  this.event_list.filter( event => event.eventStartDate > new Date());
-      // past_events = this.event_list.filter(event => event.eventEndingDate < new Date());
-      // current_events =  this.event_list.filter( event => (event.eventStartDate >= new Date() && (event.eventEndingDate <= new Date())));
-
-
     ngOnInit() {
-        this.loadAllUsers();
+        // this.loadAllUsers();
+
+        this.images = this.imagesService.getImages();
 
         if (window.screen.width < 420) { // 768px portrait
           this.mobile = true;
